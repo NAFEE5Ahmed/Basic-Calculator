e46eb1f6e5889e615809dbd9a72c5be6e482743f
predator <predatorkc4@gmail.com>
2023-06-26 14:28:53 +0500

    Operator Logic has been written

---
+++ script.js
@@ -0,0 +1,115 @@
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const multiply = document.querySelector("#multiply");
+const divide = document.querySelector("#divide");
+const percentage = document.querySelector("#percentage");
+const del = document.querySelector("#del");
+const ac = document.querySelector("#ac");
+const equal = document.querySelector("#equal");
+let inputNum = "";
+let currentInput = "";
+let operator = "";
+let storedNum = 0;
+
+for (number of numbers) {
+  number.addEventListener("click", (e) => {
+    if (inputNum.length <= 9) {
+      inputNum += e.currentTarget.textContent;
+      document.querySelector("#calculator-input").value = inputNum; // Update the input field
+      // console.log(inputNum)
+    }
+  });
+}
+
+plus.addEventListener("click", () => {
+  currentInput = document.querySelector("#calculator-input").value;
+  console.log(currentInput);
+  inputNum = "";
+  operator = "+";
+});
+
+function sumOperation(num1, num2) {
+  const result = +num1 + +num2;
+  document.querySelector("#calculator-input").value = result;
+}
+
+minus.addEventListener("click", () => {
+  currentInput = document.querySelector("#calculator-input").value;
+  console.log(currentInput);
+  inputNum = "";
+  operator = "-";
+});
+
+function minusOperation(num1, num2) {
+  const result = +num1 - +num2;
+  document.querySelector("#calculator-input").value = result;
+}
+
+multiply.addEventListener("click", () => {
+  currentInput = document.querySelector("#calculator-input").value;
+  console.log(currentInput);
+  inputNum = "";
+  operator = "*";
+});
+
+function multiplyOperation(num1, num2) {
+  const result = +num1 * +num2;
+  document.querySelector("#calculator-input").value = result;
+}
+
+divide.addEventListener("click", () => {
+  currentInput = document.querySelector("#calculator-input").value;
+  console.log(currentInput);
+  inputNum = "";
+  operator = "/";
+});
+
+function divideOperation(num1, num2) {
+  const result = +num1 / +num2;
+  document.querySelector("#calculator-input").value = result;
+}
+
+percentage.addEventListener("click", () => {
+  percentageOperation(inputNum);
+  inputNum = "";
+});
+
+// del.addEventListener("click",()=>{
+//   currentInput = document.querySelector("#calculator-input").value
+//   console.log(currentInput)
+//   inputNum = "";
+//   operator = "del"
+// })
+
+ac.addEventListener("click", () => {
+  inputNum = "";
+  document.querySelector("#calculator-input").value = "";
+});
+
+function percentageOperation(num) {
+  const result = num / 100;
+  document.querySelector("#calculator-input").value = result;
+  if (result === 0) {
+    document.querySelector("#calculator-input").value = "";
+  }
+}
+
+equal.addEventListener("click", () => {
+  if (document.querySelector("#calculator-input").value === "" || inputNum === "") {
+    document.querySelector("#calculator-input").value = "Syntax Error";
+    return;
+  }
+  if (operator === "+") {
+    sumOperation(currentInput, inputNum);
+  } else if (operator === "-") {
+    minusOperation(currentInput, inputNum);
+  } else if (operator === "*") {
+    multiplyOperationOperation(currentInput, inputNum);
+  } else if (operator === "/") {
+    divideOperation(currentInput, inputNum);
+  }
+  inputNum = "";
+  currentInput = "";
+});
